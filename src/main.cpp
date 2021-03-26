c02c040da4d58c630a22f9e171ac622082a270da
Krzysztof Adamkiewicz <k.adamkiewicz@protonmail.com>
2021-03-26 15:20:38 +0100

    Add OpenGL viewport to the window and add OpenGL related stuff to CMakeLists.txt.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 #include <spdlog/spdlog.h>
 #include "log/log.hpp"
 
@@ -6,19 +7,31 @@ int main(int argc, char* argv[])
 {
     sf::Window window(sf::VideoMode(800, 600), "tinyverse");
     log_configuration(window.getSettings());
-
-    while(window.isOpen())
+    window.setActive(true);
+    
+    bool running = true;
+    while(running)
     {
-        sf::Event e;
-        while(window.pollEvent(e))
+        sf::Event event;
+        while(window.pollEvent(event))
         {
-            switch(e.type)
+            switch(event.type)
             {
-            case sf::Event::EventType::Closed:
-                window.close();
+                case sf::Event::Closed:
+                running = false;
+                break;
+
+                // Resize OpenGL viewport when window is resized.
+                case sf::Event::Resized:
+                glViewport(0, 0, event.size.width, event.size.height);
                 break;
             }
         }
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // End the current frame (internally swaps the front and back buffers).
+        window.display();
     }
 
     return 0;
