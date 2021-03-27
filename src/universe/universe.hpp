f1371266d05ed60126e5882016f63655cd4f72ca
Krzysztof Adamkiewicz <k.adamkiewicz@protonmail.com>
2021-03-27 22:43:43 +0100

    Add ability to resize universe.

--- src/universe/universe.hpp
+++ src/universe/universe.hpp
@@ -3,22 +3,36 @@
 #ifndef UNIVERSE_HPP_INCLUDED
 #define UNIVERSE_HPP_INCLUDED
 
-#include <eigen3/Eigen/Dense>
-#include <memory>
 #include <utility>
+#include <string>
+#include <eigen3/Eigen/Dense>
+#include <spdlog/spdlog.h>
+
 #include "common_types.hpp"
 
 namespace universe
 {
     struct Universe
     {
+        Universe():
+        nbody(0)
+        {}
+
         Universe(size_t nbody)
+        {
+            set_size(nbody);
+        };
+        
+        void set_size(size_t nbody)
         {
             this->nbody = nbody;
             position.resize(nbody, 3);
             velocity.resize(nbody, 3);
             mass.resize(nbody, 1);
-        };
+            
+            spdlog::debug(std::string("Universe size set to ") +
+            std::to_string(nbody) + std::string("."));
+        }
 
         size_t nbody;
         Vectors3D position;
